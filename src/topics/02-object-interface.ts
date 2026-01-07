efea59655cda32400e362b50b471f8aeed21612d
alvaro <alvarososafrancisco1432@gmail.com>
2026-01-07 00:49:36 +0000

    first commit

---
+++ src/topics/02-object-interface.ts
@@ -0,0 +1,20 @@
+const skills: string[] = ["bash", "counter", "healing"];
+
+interface Character {
+  name: string;
+  hp: number;
+  skills: string[];
+  hometown?: string;
+}
+
+const strider: Character = {
+  name: "Strider",
+  hp: 100,
+  skills: ["bash", "counter"],
+};
+
+strider.hometown = "rivendell";
+
+console.table(strider);
+
+export {};
